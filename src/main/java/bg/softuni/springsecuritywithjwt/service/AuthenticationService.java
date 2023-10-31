363cdff54b27fe0160c5ac25779751c09ce02f02
ilambrev <ilambrev@abv.bg>
2023-10-31 20:11:13 +0200

    Authentication Service updated to check and save user tokens

--- src/main/java/bg/softuni/springsecuritywithjwt/service/AuthenticationService.java
+++ src/main/java/bg/softuni/springsecuritywithjwt/service/AuthenticationService.java
@@ -3,8 +3,11 @@ package bg.softuni.springsecuritywithjwt.service;
 import bg.softuni.springsecuritywithjwt.model.dto.AuthenticationRequest;
 import bg.softuni.springsecuritywithjwt.model.dto.AuthenticationResponse;
 import bg.softuni.springsecuritywithjwt.model.dto.RegisterRequest;
+import bg.softuni.springsecuritywithjwt.model.entity.Token;
 import bg.softuni.springsecuritywithjwt.model.entity.User;
 import bg.softuni.springsecuritywithjwt.model.enums.Role;
+import bg.softuni.springsecuritywithjwt.model.enums.TokenType;
+import bg.softuni.springsecuritywithjwt.repository.TokenRepository;
 import bg.softuni.springsecuritywithjwt.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,17 +15,21 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class AuthenticationService {
 
     private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
     private final JwtService jwtService;
     private final PasswordEncoder passwordEncoder;
     private final AuthenticationManager authenticationManager;
 
     @Autowired
-    public AuthenticationService(UserRepository userRepository, JwtService jwtService, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager) {
+    public AuthenticationService(UserRepository userRepository, TokenRepository tokenRepository, JwtService jwtService, PasswordEncoder passwordEncoder, AuthenticationManager authenticationManager) {
         this.userRepository = userRepository;
+        this.tokenRepository = tokenRepository;
         this.jwtService = jwtService;
         this.passwordEncoder = passwordEncoder;
         this.authenticationManager = authenticationManager;
@@ -36,13 +43,14 @@ public class AuthenticationService {
                 .setPassword(this.passwordEncoder.encode(request.getPassword()))
                 .setRole(Role.USER);
 
-        this.userRepository.save(user);
+        User savedUser = this.userRepository.save(user);
 
         String jwtToken = this.jwtService.generateToken(user);
 
+        saveUserToken(savedUser, jwtToken);
+
         return new AuthenticationResponse(jwtToken);
     }
-
 
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
         this.authenticationManager
@@ -52,7 +60,37 @@ public class AuthenticationService {
 
         String jwtToken = this.jwtService.generateToken(user);
 
+        revokeAllUserTokens(user);
+
+        saveUserToken(user, jwtToken);
+
         return new AuthenticationResponse(jwtToken);
+    }
+
+    private void revokeAllUserTokens(User user) {
+        List<Token> validUserTokens = this.tokenRepository.findAllValidTokensByUser(user.getId());
+
+        if (validUserTokens.isEmpty()) {
+            return;
+        }
+
+        validUserTokens.forEach(t -> {
+            t.setExpired(true);
+            t.setRevoked(true);
+        });
+
+        this.tokenRepository.saveAll(validUserTokens);
+    }
+
+    private void saveUserToken(User user, String jwtToken) {
+        Token token = new Token()
+                .setUser(user)
+                .setToken(jwtToken)
+                .setTokenType(TokenType.BEARER)
+                .setRevoked(false)
+                .setExpired(false);
+
+        this.tokenRepository.save(token);
     }
 
 }
